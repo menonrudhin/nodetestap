c4c51e46caec5b69a666d30a97216ca289a71a2c
Rudhin Menon <menonrudhin@gmail.com>
2023-04-11 21:35:50 -0400

    express js middleware code added

--- app.js
+++ app.js
@@ -2,7 +2,25 @@ const http = require('http');
 
 const express = require('express');
 
-app = express();
+const app = express();
+
+// Middleware 1
+app.use((req, res, next) => {
+	console.log('Follow the white rabbit !');
+	next(); // allows the next middleware to be executed
+});
+
+// Middleware 2
+app.use((req, res, next) => {
+	console.log('Run Neo...');
+	next();
+});
+
+// Middleware 3
+app.use((req, res, next) => {
+	console.log('Red / Blue Pill ?');
+	next();
+});
 
 const server = http.createServer(app);
 
