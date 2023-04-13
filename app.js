a73101669c0edab703846fa8474b1c089698b2a0
Rudhin Menon <menonrudhin@gmail.com>
2023-04-12 22:47:28 -0400

    handle form actions

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-// Middleware 1
-app.use('/', (req, res, next) => {
-	console.log('/ always run middleware');
-	next(); // call the next middleware which matches the url requested
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/create-user', (req, res, next) => {
+	console.log('/create-user middleware');
+	res.send('<form action="/users" method="POST" ><input type="text" name="title"><button type="submit">Create</button></input></form>');
 });
 
-// Middleware 2
 app.use('/users',(req, res, next) => {
 	console.log('/users middleware');
-	res.send('<h1>User list here</h1>');
+	const userName = req.body;
+	console.log(userName);
+	res.send('<h1>' + userName.toString() + '</h1>');
 });
 
 app.use('/', (req, res, next) => {
