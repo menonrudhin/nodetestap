d2c66beb3d0c40da8c1472dc8e34a1b09aebf113
Rudhin Menon <menonrudhin@gmail.com>
2023-09-26 21:54:06 -0400

    create note, include nav

--- routes/admin.js
+++ routes/admin.js
@@ -5,18 +5,6 @@ const router = express.Router();
 
 router.use(bodyParser.urlencoded({extended:false})); // works only with "use"
 
-router.get('/create-notes', (req, res, next) => {
-	console.log('/admin/create-notes middleware');
-	res.send('<form action="/admin/view-note" method="POST" ><input type="text" name="username"><button type="submit">Create</button></input></form>');
-});
-
-router.post('/view-note',(req, res, next) => {
-	console.log('/admin/view-note middleware');
-	const userName = req.body.username;
-	console.log(userName);
-	res.send('<h1>Note Created : ' + userName + '</h1>');
-});
-
 router.get('/app-ver', (req, res, next) => {
     res.send('<h3>v0.0.1</h3>');
 });
--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,24 @@
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const router = express.Router();
+router.use(bodyParser.urlencoded({extended:false})); // works only with "use"
 
 router.get('/notes', (req, res, next) => {
-    res.send('<h2>Hello User</h2>');
+    res.sendFile(path.join(__dirname,'../views', 'notes.html'));
+});
+
+router.get('/create-notes', (req, res, next) => {
+	console.log('/create-notes middleware');
+	res.send('<form action="/view-note" method="POST" ><input type="text" name="username"><button type="submit">Create</button></input></form>');
+});
+
+router.post('/view-note',(req, res, next) => {
+	console.log('/view-note middleware');
+	const userName = req.body.username;
+	console.log(userName);
+	res.send('<h1>Note Created : ' + userName + '</h1>');
 });
 
 module.exports = router;
