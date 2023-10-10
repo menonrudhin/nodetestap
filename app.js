f81df94a8b7c961b646de2a337fa780960dc8814
Rudhin Menon <menonrudhin@gmail.com>
2023-10-09 21:48:26 -0400

    404 page not found html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const express = require('express');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/notes');
@@ -14,7 +15,8 @@ app.get('/', (req, res, next) => {
 app.use(shopRoutes); // order matters if both have common routes
 app.use('/admin', adminRoutes);
 app.use((req, res, next) => {
-	res.status(404).send("<h1>Page Not Found</h1>");
+	res.status(404).sendFile(path.join(__dirname, 'views', 'pg-not-found.html'));
+
 });
 
 const server = http.createServer(app);
