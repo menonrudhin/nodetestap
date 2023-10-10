2587ffcd30a885dae3ec8172fdd97279eac717db
Rudhin Menon <menonrudhin@gmail.com>
2023-10-09 21:43:49 -0400

    Externalize html pages, create test note

--- routes/notes.js
+++ routes/notes.js
@@ -6,19 +6,20 @@ const router = express.Router();
 router.use(bodyParser.urlencoded({extended:false})); // works only with "use"
 
 router.get('/notes', (req, res, next) => {
-    res.sendFile(path.join(__dirname,'../views', 'notes.html'));
+    res.sendFile(path.join(__dirname, '../', 'views', 'notes.html'));
 });
 
 router.get('/create-notes', (req, res, next) => {
 	console.log('/create-notes middleware');
-	res.send('<form action="/view-note" method="POST" ><input type="text" name="username"><button type="submit">Create</button></input></form>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'add-notes.html'));
+	//res.send('<form action="/view-note" method="POST" ><input type="text" name="username"><button type="submit">Create</button></input></form>');
 });
 
 router.post('/view-note',(req, res, next) => {
 	console.log('/view-note middleware');
-	const userName = req.body.username;
-	console.log(userName);
-	res.send('<h1>Note Created : ' + userName + '</h1>');
+	const notes = req.body.notes;
+	console.log(notes);
+	res.send('<h1>Note Created : ' + notes + '</h1>');
 });
 
 module.exports = router;
