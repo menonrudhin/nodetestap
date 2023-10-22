042e3a0476121cd4f50c56a2bca1ae595e09837e
Rudhin Menon <menonrudhin@gmail.com>
2023-10-22 13:47:27 -0400

    revert git commit usage as image version

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
-#docker build . -t rudhin28/nodetestap:latest
-
-VER=`git log -1|head -1|awk '{print $2}'|cut -c1-5`
-
-echo "version : $VER"
-
-docker buildx build --platform linux/amd64 --rm . -t rudhin28/nodetestap:arch64"${VER}"
+docker buildx build --platform linux/amd64 --rm . -t rudhin28/nodetestap:arch64
 if [ $? -eq 0 ]
 then
 	echo "build successful"
-	docker push rudhin28/nodetestap:arch64"${VER}"
+	docker push rudhin28/nodetestap:arch64
 	if [ $? -eq 0 ]
 	then
 		echo "pushed successfully"
