002460d280009089f85553d94dac5349cd158189
Rudhin Menon <menonrudhin@gmail.com>
2023-10-22 13:42:10 -0400

    build script to include version from git commit hash

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #docker build . -t rudhin28/nodetestap:latest
-docker buildx build --platform linux/amd64 --rm . -t rudhin28/nodetestap:arch64
+
+VER=`git log -1|head -1|awk '{print $2}'|cut -c1-5`
+
+echo "version : $VER"
+
+docker buildx build --platform linux/amd64 --rm . -t rudhin28/nodetestap:arch64"${VER}"
 if [ $? -eq 0 ]
 then
 	echo "build successful"
-	docker push rudhin28/nodetestap:arch64
+	docker push rudhin28/nodetestap:arch64"${VER}"
 	if [ $? -eq 0 ]
 	then
 		echo "pushed successfully"
