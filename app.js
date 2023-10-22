2f1aea4c9aab6eadf14848902978c893a2d0de2c
Rudhin Menon <menonrudhin@gmail.com>
2023-10-22 13:32:45 -0400

    externalize home page

--- app.js
+++ app.js
@@ -3,20 +3,19 @@ const express = require('express');
 const path = require('path');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/notes');
+const noteRoutes = require('./routes/notes');
 
 const app = express();
 
 app.get('/', (req, res, next) => {
 	console.log('/ middleware');
-	res.send('<h1>Welcome to Q-Notes <i>v0.4</i></h1>');
+	res.sendFile(path.join(__dirname, 'views', 'home.html'));
 });
 
-app.use(shopRoutes); // order matters if both have common routes
+app.use(noteRoutes); // order matters if both have common routes
 app.use('/admin', adminRoutes);
 app.use((req, res, next) => {
 	res.status(404).sendFile(path.join(__dirname, 'views', 'pg-not-found.html'));
-
 });
 
 const server = http.createServer(app);
